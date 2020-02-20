bc6e05c5089b5ecaaf6deaf9ee20739d45d25988
yamin <myamin335@gmail.com>
2020-02-20 17:19:01 +0600

    Evening push 20-2-2020

---
+++ app/src/main/java/ltd/royalgreen/pacenet/util/FileChooser.kt
@@ -0,0 +1,16 @@
+package ltd.royalgreen.pacenet.util
+
+import android.content.Context
+import android.net.Uri
+
+class FileChooser {
+    companion object {
+        fun openFileChooser(context: Context) {
+
+        }
+
+        fun getFileOutputUri(context: Context) {
+            var uri: Uri? = null
+        }
+    }
+}
